ac017fbd5814b14d6441a0fceb68acf9f78b9316
François-Luc Haghenbeek <francois_luc.haghenbeek@utt.fr>
2018-12-19 18:25:40 +0100

    Initial Commit

---
+++ PhotoVault/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PhotoVault
+//
+//  Created by François-Luc Haghenbeek on 19/12/2018.
+//  Copyright © 2018 IF26. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
